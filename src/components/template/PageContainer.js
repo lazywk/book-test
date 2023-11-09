605a26d2b1d1abad4bbdab29067e7a63fd26ea09
lazywk <edoniyorbekdev@gmail.com>
2023-11-09 10:38:54 +0500

    book

--- src/components/template/PageContainer.js
+++ src/components/template/PageContainer.js
@@ -1,9 +1,26 @@
+
 import React from 'react'
+import { styled } from '@mui/material/styles';
+import bgimage from 'assets/images/auth-bg.png'
+
+const Container = styled('div')`
+    max-width: 1200px;
+    margin: 0 auto;
+`
+const Page = styled('div')`
+    height: 100vh;
+    background-image: url(${bgimage});
+    background-repeat: no-repeat;
+    background-color: white;
+    background-size: 70%;
+`
 
 export default function PageContainer({ children }) {
     return (
-        <div>
-            {children}
-        </div>
+        <Page>
+            <Container>
+                {children}
+            </Container>
+        </Page>
     )
 }
